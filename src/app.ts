69d5077a266547c931bde4923991cc6adccc6a30
luisscarlos <luisscarlostec@gmail.com>
2020-05-26 05:08:32 -0300

    Aplicando eager loading nos models TypeORM

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import routes from './routes';
+
 import AppError from './errors/AppError';
 
 import createConnection from './database';
@@ -13,6 +16,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
